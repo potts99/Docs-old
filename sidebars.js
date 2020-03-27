294a2f405572b39683a4a878e0e1e59972ff17b9
Jack Andrews <45759583+jwandrews99@users.noreply.github.com>
2020-03-27 16:10:43 +0000

    bugs

--- sidebars.js
+++ sidebars.js
@@ -6,6 +6,5 @@ module.exports = {
     HTML5: ['html/html-fund', 'html/html-forms', 'html/html-tables'],
     JavaScript: [],
     Css: [],
-    Backend: ['back-end/expressjs/express-routers', 'back-end/expressjs/express-routes'],
   },
 };
