2f09870aade7df4032ac3da20c91bc47db100d39
Jack Andrews <45759583+jwandrews99@users.noreply.github.com>
2020-03-29 22:40:14 +0100

    node

--- sidebars.js
+++ sidebars.js
@@ -5,6 +5,7 @@ module.exports = {
     Features: ['mdx'],
     Tools: ['gulp/gulp'],
     HTML5: ['html/html-fund', 'html/html-forms', 'html/html-tables'],
+    Microservices: ['nodejs/npm'],
     ReactJS: ['Reactjs/jsx', 'Reactjs/components', 'Reactjs/props', 'Reactjs/state'],
     JavaScript: [],
     Css: [],
